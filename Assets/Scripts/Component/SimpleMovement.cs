7763a4cd263b24853d209b59009deb4b01da5d6a
SardineFish <Sardine@live.cn>
2018-10-25 23:58:50 +0800

    Add skill animation

---
+++ Assets/Scripts/Component/SimpleMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SimpleMovement : MonoBehaviour
+{
+    public Vector2 Velocity;
+
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().velocity = Velocity;
+    }
+}
---
+++ Assets/Scripts/Render/AlignSprite.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class AlignSprite : MonoBehaviour
+{
+    public SpriteAlignment SpriteAlignment;
+    new SpriteRenderer renderer;
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+    void LateUpdate()
+    {
+        switch (SpriteAlignment)
+        {
+            case SpriteAlignment.BottomCenter:
+                transform.localPosition = Vector3.Scale(renderer.sprite.bounds.extents, new Vector3(0, 1, 0));
+                break;
+        }
+        //renderer.sprite.bounds.extents
+    }
+}
