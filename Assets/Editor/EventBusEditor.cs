98b927be07baa16ba54573cc06054aeb463a9ab3
SardineFish <Sardine@live.cn>
2018-10-26 18:38:17 +0800

    Add editor of event bus & test event responsor

---
+++ Assets/Editor/EventBusEditor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    [CustomEditor(typeof(EventBus))]
+    [CanEditMultipleObjects]
+    class EventBusEditor : UnityEditor.Editor
+    {
+        string eventName = "";
+        public override void OnInspectorGUI()
+        {
+            var eventBus = target as EventBus;
+            base.OnInspectorGUI();
+            EditorGUILayout.BeginHorizontal();
+            eventName = EditorGUILayout.TextField("Event Name", eventName);
+            if (GUILayout.Button("Dispatch"))
+            {
+                eventBus.Dispatch(eventName);
+            }
+        }
+    }
+}
