326503446a551bab5d926d459302a8bf28f43080
SardineFish <Sardine@live.cn>
2018-12-28 14:41:59 +0800

    Update cinematic script of Camp scene

--- Assets/Scripts/LuaScripts/camp.lua
+++ Assets/Scripts/LuaScripts/camp.lua
@@ -2,17 +2,25 @@ local player;
 local yellowHair;
 local redGlass;
 local twinTail;
+
+local playerWalkCoroutine;
+
 function awake()
     player = scene.entity("Player");
     yellowHair = scene.entity("YellowHair");
     redGlass = scene.entity("RedGlass");
     twinTail = scene.entity("TwinTail");
+    playerWalkCoroutine = startCoroutine(playerWalk);
+    console.log(player);
 end
 function start()
+    console.log("start");
     startCoroutine(moveCamera); 
 end
 
 function moveCamera()
+    stopCoroutine(playerWalkCoroutine);
+    coroutine.yield(waitForSeconds(1.5));
     camera.speed = 3;
     camera.acceleration = 3;
     camera.follow(twinTail);
@@ -22,4 +30,11 @@ function moveCamera()
     camera.follow(player);
     coroutine.yield(waitForSeconds(2));
     game.control(player);
+end
+
+function playerWalk()
+    while true do
+        player.move(vec2(.3, 0));
+        coroutine.yield(nil);
+    end
 end
