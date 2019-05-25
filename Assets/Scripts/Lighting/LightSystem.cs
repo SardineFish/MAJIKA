74c95ea5030893f331853a327f5473c44dd4b1a7
SardineFish <Sardine@live.cn>
2019-05-26 05:35:57 +0800

    Add level-3

--- Assets/Scripts/Lighting/LightSystem.cs
+++ Assets/Scripts/Lighting/LightSystem.cs
@@ -55,7 +55,6 @@ namespace Lighting2D
 
         public void RenderDeffer(Light2DProfile profile)
         {
-            return;
             var camera = profile.Camera;
             var cmd = profile.CommandBuffer;
             cmd.Clear();
---
+++ Assets/Scripts/LuaScripts/level-3.lua
@@ -0,0 +1,10 @@
+
+function start()
+    player = scene.entity("Player");
+    camera.follow(player);
+    game.control(player);
+end
+
+function update(dt)
+
+end
