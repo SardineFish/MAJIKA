716545c46c51b77d4b85b321ae1dd4aaa9c10f64
SardineFish <Sardine@live.cn>
2019-06-05 21:09:03 +0800

    Refactor project resources.

--- Assets/Scripts/LuaScripts/level-3.lua
+++ Assets/Scripts/LuaScripts/level-3.lua
@@ -2,8 +2,8 @@
 function start()
     boss = scene.entity("Boss-3");
     player = scene.entity("Player");
-    camera.follow(boss);
-    game.control(boss);
+    camera.follow(player);
+    game.control(player);
 end
 
 function update(dt)
