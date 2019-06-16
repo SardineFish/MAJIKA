56b07739ea4d9506cfd18dda61fbb620c722f15e
SardineFish <Sardine@live.cn>
2019-06-16 17:21:54 +0800

    Add boss-3 skills.

--- Assets/Scripts/LuaScripts/level-3.lua
+++ Assets/Scripts/LuaScripts/level-3.lua
@@ -1,6 +1,6 @@
 
 function start()
-    boss = scene.entity("Boss-3-Dragon");
+    boss = scene.entity("Boss-3");
     player = scene.entity("Player");
     camera.follow({player, boss});
     game.control(player);
