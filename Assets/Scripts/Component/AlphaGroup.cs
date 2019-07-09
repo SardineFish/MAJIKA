1808652ae67f3b8706916de00aac2d2f2bf9c89e
SardineFish <Sardine@live.cn>
2019-07-09 15:09:23 +0800

    Update boss staging animation.

---
+++ Assets/Scripts/Component/AlphaGroup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class AlphaGroup : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float Alpha = 1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponentsInChildren<SpriteRenderer>().ForEach(renderer =>
+        {
+            var color = renderer.color;
+            color.a = Alpha;
+            renderer.color = color;
+        });
+    }
+}
