e3a709bf070d37b5b9b704000712ef262a6331ed
SardineFish <Sardine@live.cn>
2019-07-16 13:45:54 +0800

    Add level 4

---
+++ Assets/Scripts/LuaScripts/level-4.lua
@@ -0,0 +1,11 @@
+
+function start()
+    resources.loadAll("Boss/Boss-4/");
+    resources.loadAll("Player/");
+    resources.loadAll("Texture/UI/");
+    resources.loadAll("Texture/Map/Level-4");
+    game.ready();
+end
+
+function update(dt)
+end
